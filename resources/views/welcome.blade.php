57f0ca6bface2f13ba0f33254530cebdde4abac2
Arseniy Sidorin <asidorin2006@gmail.com>
2023-05-20 14:28:24 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <!-- META -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- STYLES -->
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/main.css">
+    
+    <!-- PAGE-TITLE -->
+    <title>Главная</title>
+</head>
+<body>
+    <header class="main-header">
+        <div class="container">
+            <h1>OwnCloud</h1>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <h2>Ваши файлы</h2>
+            <a href="">Загрузить файл</a>
+            <div class="horz-divider"></div>
+
+            <div class="file-card">
+                <img src="img/icons/icons8-archive-folder-100.png" alt="Проект.zip">
+                <span>Проект.zip</span>
+                <div class="menu">
+                    <a href=""><img src="img/archive.png"></a>
+                    <a href=""><img src="img/edit.png"></a>
+                    <a href=""><img src="img/send-to-trash.png"></a>
+                </div>
+            </div>
+
+            <div class="file-card">
+                <img src="img/icons/icons8-image-file-100.png" alt="Скриншот1.jpg">
+                <span>Скриншот1.jpg</span>
+                <div class="menu">
+                    <a href=""><img src="img/archive.png"></a>
+                    <a href=""><img src="img/edit.png"></a>
+                    <a href=""><img src="img/send-to-trash.png"></a>
+                </div>
+            </div>
+
+            <div class="file-card">
+                <img src="img/icons/icons8-video-file-100.png" alt="Видео1.mp4">
+                <span>Видео1.mp4</span>
+                <div class="menu">
+                    <a href=""><img src="img/archive.png"></a>
+                    <a href=""><img src="img/edit.png"></a>
+                    <a href=""><img src="img/send-to-trash.png"></a>
+                </div>
+            </div>
+            
+        </div>
+    </main>
+    
+</body>
+</html>
